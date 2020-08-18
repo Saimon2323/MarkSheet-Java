95ce183a3bf07ccaa27a35b8bac08e16e01d1125
Saimon <saimon.ctg@gmail.com>
2020-08-18 10:46:32 +0600

    Generate mark sheet in a single file

--- src/GenerateMarkSheet.java
+++ src/GenerateMarkSheet.java
@@ -1,57 +1,19 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Logger;
-
-/**
- * @Author Muhammad Saimon
- * @since Aug 8/16/20 10:48 AM
- */
 
 public class GenerateMarkSheet {
+    public static void main(String[] args) {
 
-    private static final Logger logger = Logger.getLogger(GenerateMarkSheet.class.getName());
 
-    private File createDirectory(String fileName) {
-        File inputFolder = new File(fileName);
-        if (!inputFolder.exists()) {
-            boolean mkdir = inputFolder.mkdir();
-            if (!mkdir) {
-                logger.info("Can't make directory!");
-            }
-        } else {
-            logger.info("Folder already created!");
-        }
-        return inputFolder;
-    }
-
-    public File inputMarks(List<String> studentInfo) {
-        File inputFolder = createDirectory("Input Marks");
-
-        try {
-            File inputMarksFile = new File(inputFolder.getPath() + "/marks.txt");
-            inputMarksFile.createNewFile();
-
-            PrintWriter printWriter = new PrintWriter(inputMarksFile);
-            studentInfo.forEach(printWriter::println);
-            printWriter.close();
-            return inputMarksFile;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new File("");
-        }
-
-    }
-
-    public void precessAndGenerateMarkSheet(File inputFileDir) {
+        //File file = new File("/home/saimon/workspace/saimon-project/Generate_Mark_Sheet/src/marks.txt");
+        File file = new File("src/marks.txt");
         String[] subjects = new String[10];
         boolean firstRow = true;
 
         try {
-            Scanner myReader = new Scanner(inputFileDir);
+            Scanner myReader = new Scanner(file);
             while (myReader.hasNextLine()) {
                 if (firstRow) {
                     firstRow = false;
@@ -62,17 +24,16 @@ public class GenerateMarkSheet {
                 String data = myReader.nextLine();
                 String[] studentData = data.split(",");
 
-                double banglaGp   = getGp(studentData[2]);
-                double englishGp  = getGp(studentData[3]);
-                double mathGp     = getGp(studentData[4]);
+                double banglaGp = getGp(studentData[2]);
+                double englishGp = getGp(studentData[3]);
+                double mathGp = getGp(studentData[4]);
                 double religionGp = getGp(studentData[5]);
 
-                File inputFolder = createDirectory("Output");
-                String outputFile = inputFolder.getPath() + "/" + studentData[0] + "-" + studentData[1] + ".txt";
+                String outputFile = "src/" + studentData[0] + "-" + studentData[1] + ".txt";
 
                 try {
                     PrintWriter outputStream = new PrintWriter(outputFile);
-                    outputStream.format("%nName: %s Student Role: %s %n %n", studentData[1], studentData[0]);
+                    outputStream.format("%nName: %s. Student Roll: %s %n %n", studentData[1], studentData[0]);
                     outputStream.println("----------------------------------------------");
                     outputStream.format("%-8s | %-8s | %-8s | %-8s | %n", "Subject", "Marks", "Grade Point", "Grade");
                     outputStream.println("----------------------------------------------");
@@ -97,26 +58,7 @@ public class GenerateMarkSheet {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    private static double getGp(String num) {
-        int number = Integer.parseInt(num);
-        if (number < 33) {
-            return 0;
-        }
-        else if (number < 40) {
-            return 1;
-        } else if (number < 50) {
-            return 2;
-        } else if (number < 60) {
-            return 3;
-        } else if (number < 70) {
-            return 3.5;
-        } else if (number < 80) {
-            return 4;
-        } else {
-            return 5;
-        }
     }
 
     private static double getGpa(double banglaGp, double englishGp, double mathGp, double religionGp) {
@@ -143,5 +85,24 @@ public class GenerateMarkSheet {
         }
     }
 
-
+    private static double getGp(String num) {
+        int number = Integer.parseInt(num);
+        if (number < 33) {
+            return 0;
+        }
+        else if (number < 40) {
+            return 1;
+        } else if (number < 50) {
+            return 2;
+        } else if (number < 60) {
+            return 3;
+        } else if (number < 70) {
+            return 3.5;
+        } else if (number < 80) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
 }
+
